7ed3d84043f11285fdfdb6c97c36696207ec1692
alonziv1 <82515685+alonziv1@users.noreply.github.com>
2022-10-26 15:07:33 +0300

    kirby update

--- Kirby/Kirby.py
+++ Kirby/Kirby.py
@@ -1,10 +1,38 @@
+import os
+from Kirby.Enums import Hospital, Staining 
+import Slide
+import validationChecks as vChecks
+import Enums
 
 
 
 
 class Kirby: 
 
-    def __init__(self, dir_path, hospital, staining, dateset, batch):
+    def __init__(self, dir_path: str, hospital : Hospital, staining : Staining, dateset : Dateset , batch : int):
+
+        try: 
+
+            vChecks.check_arguments(dir_path , hospital, staining, dateset, batch)
+
+        except:
+            print("An exception occurred")
+            
+
+
+
+        
+
+        self.dir_path = dir_path
+        self.hospital = hospital
+        self.staining = staining
+        self.dataset = dateset 
+        self.batch = batch
+    
+    def readSlides(self):
+
+
+
 
 
 
